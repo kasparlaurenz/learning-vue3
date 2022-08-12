caf54fefb69626115b7f656ac7832e044264ec45
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-08-12 12:03:30 +0200

    default slot

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="backdrop" @click.self="closeModal" :class="{ open: showModal }">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <slot></slot>
+      <slot>Default content only shown if nothing passed</slot>
       <div class="actions">
         <slot name="links"></slot>
       </div>
